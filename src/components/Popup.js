88badebc8a5fb14542e61029bddc0676128873ed
Madhura <madhura.bhujbal@gmail.com>
2021-03-30 16:21:50 +0100

    Create a new component and add code to it

---
+++ src/components/Popup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Popup(props) {
+    return (props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <button className="close-btn">close</button>
+            </div>
+        </div>
+    ) : "";
+}
+
+export default Popup
