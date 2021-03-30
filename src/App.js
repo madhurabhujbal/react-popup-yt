d2951af19bd4f23fdcfa09637ffe2974d111566e
Madhura <madhura.bhujbal@gmail.com>
2021-03-30 17:11:50 +0100

    Add code for open and close popup by clicking button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Popup from './components/Popup';
+import { useState } from 'react';
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <div className="App">
       <main>
         <h1>React Popups!</h1>
         <br></br>
-        <button>Open Popup</button>
+        <button onClick={() => setButtonPopup(true)}>Open Popup</button>
       </main>
-      <Popup trigger={true}>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
         <h3>My popup</h3>
         <p>This is my button triggered popup</p>
       </Popup>
